b04f9c785f6e2d01f9bf994046b1cadccd9b7cac
grissomlin <grissomlin643@gmail.com>
2026-03-02 14:59:09 +0800

    Create india_config.py

---
+++ markets/india/india_config.py
@@ -0,0 +1,15 @@
+# markets/india/india_config.py
+# -*- coding: utf-8 -*-
+"""
+Alias module.
+
+downloader.py expects:
+    from .india_config import ...
+
+But the real config file is:
+    markets/india/in_config.py
+"""
+
+from __future__ import annotations
+
+from .in_config import *  # noqa: F401,F403
